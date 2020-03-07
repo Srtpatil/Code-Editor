763068b55ea1157d6222982860ad2593335abe4a
Srtpatil <samarth.17u512@viit.ac.in>
2020-03-07 23:19:29 +0530

    import edited

--- src/component/editor/editor.js
+++ src/component/editor/editor.js
@@ -1,11 +1,11 @@
 import CodeMirror from "react-codemirror";
 import React, { Component } from "react";
 import "./editor.css";
-require("codemirror/mode/xml/xml");
-require("codemirror/mode/javascript/javascript");
-require("codemirror/lib/codemirror.css");
-require("codemirror/theme/dracula.css");
-require("codemirror/addon/edit/closetag");
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/xml/xml";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/dracula.css";
+import "codemirror/addon/edit/closetag";
 
 class Editor extends Component {
   render() {
