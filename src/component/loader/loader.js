89f8c22514b8780e93b65c0a88089586fa59e44d
Srtpatil <samarth.17u512@viit.ac.in>
2020-03-11 13:26:48 +0530

    loader added

---
+++ src/component/loader/loader.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./loader.css";
+const Loader = () => {
+  return (
+    <div class="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
