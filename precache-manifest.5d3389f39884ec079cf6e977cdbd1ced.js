853075b004a5e11c3d41e556b320392f00974a7d
Samarth <samarth@pop-os.localdomain>
2020-05-15 10:42:15 +0530

    Updates

---
+++ precache-manifest.5d3389f39884ec079cf6e977cdbd1ced.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0d6c7bce29a950df98647e09dbe011",
+    "url": "/Code-Editor/index.html"
+  },
+  {
+    "revision": "309b59b1e950453eba56",
+    "url": "/Code-Editor/static/css/2.cec23dce.chunk.css"
+  },
+  {
+    "revision": "0ab94981b569806ecb22",
+    "url": "/Code-Editor/static/css/main.1085bd71.chunk.css"
+  },
+  {
+    "revision": "309b59b1e950453eba56",
+    "url": "/Code-Editor/static/js/2.80f57cab.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/Code-Editor/static/js/2.80f57cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ab94981b569806ecb22",
+    "url": "/Code-Editor/static/js/main.049faa4b.chunk.js"
+  },
+  {
+    "revision": "9bd39dcb5038ba427b78",
+    "url": "/Code-Editor/static/js/runtime-main.60939093.js"
+  }
+]);
